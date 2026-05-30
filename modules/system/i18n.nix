370c30d3e4c566a0dd4b540bc07e57cb100f5fab
Mooling0602 <clemooling@outlook.com>
2026-05-30 17:56:38 +0800

    fix: enable fcitx5 waylandFrontend for Wayland input method protocol

--- modules/system/i18n.nix
+++ modules/system/i18n.nix
@@ -20,6 +20,7 @@
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
+    fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       qt6Packages.fcitx5-chinese-addons
       fcitx5-gtk
