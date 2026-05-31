9707064b05d84d07df37de3ab818f04c1f55b221
Mooling0602 <clemooling@outlook.com>
2026-05-31 13:53:50 +0800

    fix: use seahorse ssh-askpass instead of openssh-askpass

--- hosts/mooling-laptop/home.nix
+++ hosts/mooling-laptop/home.nix
@@ -23,7 +23,7 @@
     QT_QPA_PLATFORMTHEME_QT6 = "qt6ct";
     QT_WAYLAND_DECORATION = "ssd";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    SSH_ASKPASS = "${pkgs.openssh-askpass}/bin/ssh-askpass";
+    SSH_ASKPASS = "${pkgs.seahorse}/libexec/seahorse/ssh-askpass";
   };
 
   # XDG Portal 偏好：KDE 优先
@@ -119,7 +119,7 @@
     glow # markdown previewer in terminal
     lazygit # terminal git UI
     gh # GitHub CLI
-    openssh-askpass  # SSH 密码弹窗
+    seahorse  # SSH 密钥弹窗
 
     btop  # replacement of htop/nmon
     iotop # io monitoring
