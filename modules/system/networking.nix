5be5a772ae0148f8f13c8ed0f811d310bc1cbc3f
Mooling0602 <clemooling@outlook.com>
2026-09-13 15:57:06 +0800

    fix: optimize clash-verge-service

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   networking.firewall.enable = false; # For normal users, firewall is not so useful and causes many errors
@@ -13,5 +13,20 @@
     enable = true;
     # serviceMode = true;
     tunMode = true;
-  }; # Magical cat that can ignore GFW~
+  }; # Magical cat that can bypass GFW~
+
+  systemd.services.clash-verge = {
+    environment.XDG_STATE_HOME = "/run"; # -> /run/clash-verge-service/desired-state.json
+    serviceConfig = {
+      RuntimeDirectory = lib.mkForce [
+        "clash-verge-rev"
+        "clash-verge-service"
+      ];
+      RuntimeDirectoryPreserve = "restart";
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "r /var/lib/clash-verge-service/desired-state.json"
+  ];
 }
